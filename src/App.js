90bf17b9dafddb3e6ab85eb0a750b205a2e8ed6a
nhaterik <nhaterik@gmail.com>
2024-04-27 10:37:55 +0700

    router,layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { publicRoutes } from "~/routes";
+import {DefaultLayout} from "~/components/Layout";
+import { Fragment } from "react";
 function App() {
   return (
-    <div className="App">
-     <h1> React app </h1>
-    </div>
+    <Router>
+      <div className="App">
+       <Routes>
+        {publicRoutes.map((route,idx)=>{
+          var Layout=DefaultLayout
+          if(route.layout)
+          Layout=route.layout
+          else if(route.layout===null)
+          Layout=Fragment
+          const Page=route.component
+          return <Route key={idx} path={route.path} element={
+            <Layout>
+              <Page/>
+            </Layout>
+        } />
+        })}
+       </Routes>
+      </div>
+    </Router>
   );
 }
 
