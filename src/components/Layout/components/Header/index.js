3aea037f1b895353ed16ba909489df9d6a7e81d7
nhaterik <nhaterik@gmail.com>
2024-05-01 13:50:07 +0700

    Button module

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,15 +4,16 @@ import styles from './Header.module.scss'
 import Tippy from '@tippyjs/react/headless'
 import { Wrapper as PopperWrapper} from '~/components/Popper'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark ,faSpinner,faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark ,faSpinner,faMagnifyingGlass,faSignIn,faSignOut} from '@fortawesome/free-solid-svg-icons'
 import images from '~/assets/images'
 import AccountItem from '~/components/AccountItem'
+import Button  from '~/components/Button'
 const cx=classNames.bind(styles)
 function Header( ) {
    const [searchResult,setSearchResult]=useState([]);
    useEffect(()=>{
     setTimeout(()=>{
-      setSearchResult([1,2,3]);
+      setSearchResult([]);
       console.log('hello')
     },0);
    },[]);
@@ -51,7 +52,13 @@ function Header( ) {
               </button>
             </div>
               </Tippy>
-        <div className={cx('actions')}></div>
+        <div className={cx('actions')}>
+          <Button text >
+            Upload</Button>
+          <Button primary >
+            Login 
+          </Button>
+           </div>
 
         </div>
       </header>
