780dc1cbf56995bb50b12984190923b7a30e8fd4
PHAM_VAN_NHAT <nhaterik@gmail.com>
2024-05-23 18:46:42 +0700

    rebult and construct #1

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import HeadelessTip from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
-import * as searchServices from '~/apiServices/searchServices'
+import * as searchServices from '~/services/searchService'
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
